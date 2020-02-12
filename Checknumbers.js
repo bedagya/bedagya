1af563eb466bce85435bdccd2a38311b3d3096fb
Bedagya <bedagya123@gmail.com>
2020-02-13 01:42:28 +0545

    h

---
+++ Checknumbers.js
@@ -0,0 +1,8 @@
+var a=parseInt(prompt("Enter a number:"));
+        var b=parseInt(prompt("Enter another number:"));
+        if(a==50 || b==50 || (a+=b)==50){
+            alert("TRUE")
+        }    
+        else{
+            alert("FALSE")
+        }
